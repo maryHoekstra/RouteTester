cd89024070ac2e4d749045200090862c42c24386
Mary Hoekstra <mary.hoekstra@queensu.ca>
2017-11-11 16:09:20 -0500

    Created segue from "log commute" button to NewCommuteViewController

---
+++ RouteTester/NewCommuteViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewCommuteViewController.swift
+//  RouteTester
+//
+//  Created by Mary Hoekstra on 2017-11-09.
+//  Copyright © 2017 Mary Hoekstra. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+// view that allows users to log a new commute
+class NewCommuteViewController: UIViewController {
+
+
+
+}
