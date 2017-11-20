164a72c69da384401c683c5711ec5a4eecd9aefe
Mary Hoekstra <mary.hoekstra@queensu.ca>
2017-11-20 12:33:10 -0500

    Added map view to commute details page - route is not currently displaying.

--- RouteTester/ViewControllers/CommuteDetailsViewController.swift
+++ RouteTester/ViewControllers/CommuteDetailsViewController.swift
@@ -7,20 +7,23 @@
 //
 
 import UIKit
+import MapKit
 
 class CommuteDetailsViewController: UIViewController {
+    
+
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
     
     var commute: Commute!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureView()
 
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     private func configureView() {
@@ -33,7 +36,86 @@ class CommuteDetailsViewController: UIViewController {
         distanceLabel.text = "Distance:  \(formattedDistance)"
         dateLabel.text = formattedDate
         timeLabel.text = "Time:  \(formattedTime)"
+        
+        loadMap()
+    }
+    
+    // set region for map
+    private func mapRegion() -> MKCoordinateRegion? {
+        guard
+            let locations = commute.locations,
+            locations.count > 0
+            else {
+                return nil
+        }
+        
+        let latitudes = locations.map { location -> Double in
+            let location = location as! Location
+            return location.latitude
+        }
+        
+        let longitudes = locations.map { location -> Double in
+            let location = location as! Location
+            return location.longitude
+        }
+        
+        let maxLat = latitudes.max()!
+        let minLat = latitudes.min()!
+        let maxLong = longitudes.max()!
+        let minLong = longitudes.min()!
+        
+        let center = CLLocationCoordinate2D(latitude: (minLat + maxLat) / 2,
+                                            longitude: (minLong + maxLong) / 2)
+        let span = MKCoordinateSpan(latitudeDelta: (maxLat - minLat) * 1.3,
+                                    longitudeDelta: (maxLong - minLong) * 1.3)
+        return MKCoordinateRegion(center: center, span: span)
+    }
+    
+    // create overlay
+    // convert locations from commute into objects used by MKPolyline
+    private func polyLine() -> MKPolyline {
+        guard let locations = commute.locations else {
+            return MKPolyline()
+        }
+        
+        let coords: [CLLocationCoordinate2D] = locations.map { location in
+            let location = location as! Location
+            return CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+        }
+        return MKPolyline(coordinates: coords, count: coords.count)
+    }
+    
+    // ensure there are locations in commute object, then create overlay and display
+    private func loadMap() {
+        guard
+            let locations = commute.locations,
+            locations.count > 0,
+            let region = mapRegion()
+            else {
+                let alert = UIAlertController(title: "Error",
+                                              message: "Sorry, this commute has no locations saved",
+                                              preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+                present(alert, animated: true)
+                return
+        }
+        
+        mapView.setRegion(region, animated: true)
+        mapView.add(polyLine())
     }
     
 
+
+}
+// delegate method to display map overlay
+extension CommuteDetailsViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        guard let polyline = overlay as? MKPolyline else {
+            return MKOverlayRenderer(overlay: overlay)
+        }
+        let renderer = MKPolylineRenderer(polyline: polyline)
+        renderer.strokeColor = .black
+        renderer.lineWidth = 3
+        return renderer
+    }
 }
